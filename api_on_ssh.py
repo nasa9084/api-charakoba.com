c7578dc952ff01116d285ff7ffd18dac18b058a9
nasa9084 <nasa9084@live.jp>
2016-06-22 14:36:27 +0900

    add: is_alive function
    to check API on SSH server is dead or alive

--- api_on_ssh.py
+++ api_on_ssh.py
@@ -5,3 +5,13 @@ import json
 
 with open('config.json', 'r') as f:
     cfg = json.load(f)
+
+
+def is_alive():
+    res = requests.get(
+        cfg['IN_API']['ENDPOINT']
+    )
+    if res.status_code == 200:
+        return True
+    else:
+        return False
