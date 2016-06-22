2dbd001939a767f86c8e19c9d15c1f423e376163
nasa9084 <nasa9084@live.jp>
2016-06-22 14:56:36 +0900

    add: modify_publickey function
    to modify SSH user's registered Public key

--- api_on_ssh.py
+++ api_on_ssh.py
@@ -48,3 +48,20 @@ def delete_user(username):
         return True
     else:
         return False
+
+
+def modify_publickey(username, publickey):
+    payload = {
+        'mode': 'mod',
+        'apikey': cfg['IN_API']['APIKEY'],
+        'user': username,
+        'publickey': publickey
+    }
+    res = requests.post(
+        cfg['IN_API']['ENDPOINT'],
+        data=payload
+    )
+    if res.status_code == 200 and res.text == 'Succeeded':
+        return True
+    else:
+        return False
