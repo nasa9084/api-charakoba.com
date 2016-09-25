84cf87e876b7c7264835ac912005de4e41bb2b27
nasa9084 <nasa9084@live.jp>
2016-09-25 21:05:10 +0900

    add: BaseRecord(s) classes, DNSRecord(s) classes

--- common.py
+++ common.py
@@ -2,9 +2,11 @@
 # -*- coding:utf-8 -*-
 
 # Library
+from abc import abstractmethod, ABCMeta
 from bottle import HTTPError, request
 import functools
 from hashlib import md5 as encode, sha512 as encrypt
+import json
 import pymysql as DB
 from pymysql.cursors import DictCursor as DC
 import re
@@ -21,6 +23,12 @@ class ParameterRequirementsError(HTTPError):
     def __init__(self, key):
         msg = messages.REQUIRE.format(key)
         super.__init__(400, msg)
+
+
+class RecordNotFoundError(HTTPError):
+    def __init__(self, rid):
+        msg = messages.RECORD_NOT_FOUND + ': ' + str(rid)
+        super().__init__(404, msg)
 
 
 class UserNotActivatedError(HTTPError):
@@ -143,6 +151,127 @@ class User(object):
                 redis.delete(key)
         redis.setex(token, self.uid, config.APIKEY_TTL*60*60)
         return token
+
+
+class BaseRecords(object, metaclass=ABCMeta):
+    def __init__(self):
+        self.table
+
+    def json(self):
+        '''List all Records and return json'''
+        with DB.connect(cursorclass=DC, **config.RDB_INFO) as cursor:
+            cursor.execute(
+                'SELECT * FROM {};'.format(self.table)
+            )
+            result = cursor.fetchall()
+        return json.dumps(result)
+
+    def delete(self, id_):
+        '''
+        :param int id_: Record ID you want to delete
+        '''
+        with DB.connect(**config.RDB_INFO) as cursor:
+            cursor.execute(
+                'DELETE FROM {} '
+                'WHERE id=%s;'.format(self.table),
+                (id_)
+            )
+
+    @abstractmethod
+    def add(self):
+        '''
+        :returns: 追加されたレコードオブジェクト
+        :rtype: Record
+        '''
+        raise NotImplementedError
+
+
+class BaseRecord(object, metaclass=ABCMeta):
+    def __init__(self, id_):
+        self.id_ = id_
+
+    @abstractmethod
+    def __str__(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self):
+        raise NotImplementedError
+
+
+class DNSRecords(BaseRecords):
+    '''DNS Records class extends Base Records'''
+    def __init__(self):
+        self.table = 'dns'
+
+    def add(self, type_, host, domain, ipv4_addr):
+        '''
+        :param str type_: DNS Record Type
+        :param str host: Host name
+        :param str domain: domain name
+        :param str ipv4_addr: IPv4 address
+        :return: DNSRecord Object that created
+        :rtype: DNSRecord
+        '''
+        with DB.connect(**config.RDB_INFO) as cursor:
+            cursor.execute(
+                'INSERT INTO dns '
+                '(type, host, domain, ipv4_addr) '
+                'VALUES (%s, %s, %s, %s);',
+                (type_, host, domain, ipv4_addr)
+            )
+        return DNSRecord(cursor.lastrowid)
+
+
+class DNSRecord(BaseRecord):
+    '''DNS Record Class extends BaseRecord'''
+    def __init__(self, id_):
+        with DB.connect(cursorclass=DC, **config.RDB_INFO) as cursor:
+            cursor.execute(
+                'SELECT type, host, domain, ipv4_addr '
+                'FROM dns '
+                'WHERE id=%s;',
+                (id_, )
+            )
+            result = cursor.fetchone()
+        if not result:
+            raise RecordNotFoundError(id_)
+        self.type_ = result['type']
+        self.host = result['host']
+        self.domain = result['domain']
+        self.ipv4_addr = result['ipv4_addr']
+        super().__init__(id_)
+
+    def __str__(self):
+        return '[DNS {} Record ID:{}] {}.{} -> {}'.format(
+            self.type_, self.id_, self.host, self.domain, self.ipv4_addr
+        )
+
+    def update(self, type_=None, host=None, domain=None, ipv4_addr=None):
+        '''
+        :param type_: Record type e.g. A, MX, ...
+        :type type_: str or None
+        :param host: hostname that resolve from
+        :type host: str or None
+        :param domain: domain that resolve from
+        :type domain: str or None
+        :param ipv4_addr: ipv4 address that resolve to
+        :type ipv4_addr: str or None
+        '''
+        with DB.connect(**config.RDB_INFO) as cursor:
+            cursor.execute(
+                'UPDATE dns '
+                'SET type=%s, host=%s, domain=%s, ipv4_addr=%s '
+                'WHERE id=%s;',
+                (
+                    type_ if type_ else self.type_,
+                    host if host else self.host,
+                    domain if domain else self.domain,
+                    ipv4_addr if ipv4_addr else self.ipv4_addr,
+                    self.id_
+                )
+            )
+        return self
 
 
 def params(require=[], option=[]):
--- messages.py
+++ messages.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
 REQUIRE = "{0} is Required."
+RECORD_NOT_FOUND = "Record Not Found"
 USER_NOT_FOUND = "User Not Found"
 USER_NOT_ACTIVATED = "User Not Activated"
