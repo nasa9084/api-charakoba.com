3a19ef0be55b8f9e17d3e865349cb9933cee3d88
nasa9084 <nasa9084@live.jp>
2016-10-05 01:15:50 +0900

    fix: default_body to only str

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -24,34 +24,34 @@ class CharakobaError(HTTPError):
 
 class AuthenticationError(CharakobaError):
     default_status = 401
-    default_body = {'message': msg.AUTH_ERROR}
+    default_body = msg.AUTH_ERROR
 
 
 class ParameterRequirementsError(CharakobaError):
     default_status = 400
-    default_body = {'message': msg.PARAM_ERROR}
+    default_body = msg.PARAM_ERROR
 
 
 class PermissionError(CharakobaError):
     default_status = 403
-    default_body = {'message': msg.PERMISSION_ERROR}
+    default_body = msg.PERMISSION_ERROR
 
 
 class RecordNotFoundError(CharakobaError):
     default_status = 404
-    default_body = {'message': msg.RECORD_NOT_FOUND_ERROR}
+    default_body = msg.RECORD_NOT_FOUND_ERROR
 
 
 class TokenError(CharakobaError):
     default_status = 401
-    default_body = {'message': msg.TOKEN_ERROR}
+    default_body = msg.TOKEN_ERROR
 
 
 class UserNotActivatedError(CharakobaError):
     default_status = 403
-    default_body = {'message': msg.USER_NOT_FOUND_ERROR}
+    default_body = msg.USER_NOT_FOUND_ERROR
 
 
 class UserNotFoundError(HTTPError):
     default_status = 404
-    default_body = {'message': msg.USER_NOT_FOUND_ERROR}
+    default_body = msg.USER_NOT_FOUND_ERROR
