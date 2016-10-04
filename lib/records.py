3c47f48d993270a197bd1f4980eb240791266fd3
nasa9084 <nasa9084@live.jp>
2016-10-04 23:33:35 +0900

    fix: column name modified from type_ to type

--- lib/records.py
+++ lib/records.py
@@ -7,7 +7,7 @@ from lib.superclass import BaseRecord
 class DNSRecord(BaseRecord):
     '''DNS Record Class'''
     tablename = 'dns'
-    columns = ['type_', 'host', 'domain', 'ipv4_addr']
+    columns = ['type', 'host', 'domain', 'ipv4_addr']
 
 
 class ReverseProxyRecord(BaseRecord):
