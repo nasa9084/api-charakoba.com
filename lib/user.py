c779e8ba75deb3aebae10ce26a2e881382d44337
nasa9084 <nasa9084@live.jp>
2016-10-06 11:40:03 +0900

    fix: wrong col name

--- lib/user.py
+++ lib/user.py
@@ -105,7 +105,7 @@ class User(object):
         with DB.connect(**config.mysql) as cursor:
             cursor.execute(
                 'UPDATE users '
-                'SET is_activate=1 '
+                'SET is_active=1 '
                 'WHERE id=%s;',
                 (self.id_,)
             )
